f35c3f32519268401a9e43cf8e01f39eb11604f4
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-17 14:17:22 -0700

    programs 5,6 print numbers with printf and putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints numbers in asc order with printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
