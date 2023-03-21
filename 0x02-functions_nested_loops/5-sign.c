3934ecd510006c531dc8bd367cc927a4f8868196
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-21 15:31:31 -0700

    fix program 5 by ammending function signature

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _print_sign - prints the sign of input n
+ * print_sign - prints the sign of input n
  * @n: Input integer
  * Return: 1 for +ve, 0 for 0, -1 for -ve
  */
-int _print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
