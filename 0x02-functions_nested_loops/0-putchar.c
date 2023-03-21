7c3be56342cf8e2dd2f6baeed14aecaff6efa4ae
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-21 12:47:31 -0700

    Task 0, implementing a C program to print putchar using custom putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * uses custom function _putchar to print chars
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
+	return (0);
+}
