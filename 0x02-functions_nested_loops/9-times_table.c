6b18002f343e636a880ad1e33f3810b1c55b6016
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-21 18:57:29 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,15 +2,15 @@
 
 /**
  * times_table - prints time table 0 to 9
- * @a: Input integer
- * @b: Input integer
  * Return: void
  */
 void times_table(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			int mul = i * j;
 			int first_digit = mul / 10;
