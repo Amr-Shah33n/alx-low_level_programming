70f9c34f9bccc28c59ddd4143073f2dd6c6aa3a2
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-21 17:23:13 -0700

    Task 7, print and return last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ *
+ * @n: Input integer
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int rem = n % 10;
+
+	int abs_rem = _abs(rem);
+
+	_putchar('0' + abs_rem);
+	return (abs_rem);
+}
