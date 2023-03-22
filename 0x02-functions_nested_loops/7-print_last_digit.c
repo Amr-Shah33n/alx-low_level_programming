89c3b9bc9eeb437c2578c253c78bf2f5d8d58068
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-21 17:34:38 -0700

    fixing program 7 by implement the _abs function in program 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,15 @@ int print_last_digit(int n)
 {
 	int rem = n % 10;
 
-	int abs_rem = _abs(rem);
-
+	int abs_rem;
+	if (rem >= 0)
+	{
+		abs_rem = rem;
+	}
+	else
+	{
+		abs_rem = rem * -1;
+	}
 	_putchar('0' + abs_rem);
 	return (abs_rem);
 }
