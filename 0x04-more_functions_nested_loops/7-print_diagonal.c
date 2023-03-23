59380f8ca9a23b5b98ad4f36dbc98631a361176e
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-23 16:13:17 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints triangle with size
+ * @n: Int diagonal size
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+			{
+				_putchar(92);
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar(10);
+	}
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+}
