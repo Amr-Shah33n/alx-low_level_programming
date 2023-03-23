9a47e05879a4b692a36bef6ae31ac71b67e241ef
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-23 15:56:56 -0700

    Fixed task 10 logic, removed unnecassry files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,9 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (j < size - i)
+			if (j < size - i - 1)
 			{
 				_putchar(' ');
 			}
