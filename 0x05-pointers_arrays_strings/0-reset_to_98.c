1d1e09d9848a8e009adf9546b5f016e95346c0df
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-27 18:18:52 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value to 98
+ * @n: pointer to input int
+ *
+ * Return: void return, sets value only
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
