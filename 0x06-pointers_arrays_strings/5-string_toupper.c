cca35c236c02ab9f3982319d07995047394ff2a8
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-30 15:49:51 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * str_toupper - transfor string to upper case
+ * @dest: string input
+ * Return: pointer to uppercase string
+ */
+char *str_toupper(char *dest)
+{
+	char *start = dest;
+
+	while (*dest != '\0')
+	{
+		if (*dest >= 97 && *dest <= 122)
+		{
+			*dest = *dest - 32;
+		}
+		dest++;
+	}
+	return (start);
+}
