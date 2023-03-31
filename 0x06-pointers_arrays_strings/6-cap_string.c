bf1996ef3b986b7374ebfcc90812a095ec96cb96
amrshaheen <amr.shaheen0000@gmail.com>
2023-03-30 18:33:33 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string -Capatilizes words in a string
+ * @dest: string input
+ * Return: pointer to uppercase string
+ */
+char *cap_string(char *dest)
+{
+	char *start = dest;
+
+	while (*dest != '\0')
+	{
+		if ((*dest == '\t' || *dest == '\n' || *dest == ' ' || *dest == ';'
+					|| *dest == '.' || *dest == ',' || *dest == '(' || *dest == ')'
+					|| *dest == '{' || *dest == '}'
+					|| *dest == '?' || *dest == '"') && *(dest + 1) != '\0')
+		{
+			if (*(dest + 1) >= 97 && *dest <= 122)
+			{
+				*(dest + 1) = *(dest + 1) - 32;
+			}
+		}
+		dest++;
+	}
+	return (start);
+}
