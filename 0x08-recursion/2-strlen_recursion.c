29a744ee4d6c7acace7a2656f9f4abfd8317f39d
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-05 11:03:03 -0700

    Task 2, use recursion to get the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * _strlen_recursion - gets length of input string
+ * @s: input string
+ *
+ * Return: length of input string
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+}
