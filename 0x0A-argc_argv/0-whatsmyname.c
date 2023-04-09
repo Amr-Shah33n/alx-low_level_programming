8bdfdf75d7a2541bb13ad6cd2df7af23f7c19989
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-09 05:15:10 -0700

    Initial commit, readme and task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program's name
+ * @argc: argument count
+ * @argv: array of input arguments
+ * Return: 0 on success.
+ **/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Projec 0x0A. C - argc, argv. Exercises for main arguments in C.
