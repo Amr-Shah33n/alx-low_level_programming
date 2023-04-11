0928599b58ff2730fe1d05d0c823bb4896ddafd8
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-11 15:32:27 -0700

    Fix betty styling

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concates two input strings
- * @s1: string input 1
- * @s2: string input 2
- * Return: pointer to concatenated string
+ * alloc_grid - concates two input strings
+ * @width: array width
+ * @height: array height
+ * Return: pointer to allocated 2D memory array
  **/
 int **alloc_grid(int width, int height)
 {
 	int **a;
 	int i, j, k;
 
-	a = malloc(height * sizeof(int*));
-	if ( a == NULL)
+	a = malloc(height * sizeof(int *));
+	if (a == NULL)
 	{
 		return (NULL);
 	}
