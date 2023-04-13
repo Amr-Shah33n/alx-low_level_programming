1f003e6103802f606e7b615a016aed7a8cf1850e
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-13 12:55:56 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - returns array with allocatoed memory
+ * @nmeb: number of elements
+ * @size: size of each element
+ * Return: pointer to array or NULL on failure
+ **/
+void *_calloc(unsigned int nmeb, unsigned int size)
+{
+	void *a;
+	char *p;
+	unsigned int i = 0;
+
+	if (nmeb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * nmeb);
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	p = a;
+	while (i < nmeb)
+	{
+		p[i++] = 0;
+	}
+	return (a);
+}
