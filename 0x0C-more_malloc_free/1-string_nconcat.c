50d0c9c0fc56ca8d6d2f20fb64473f7bb06bb9af
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-13 09:22:06 -0700

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - gets length of input string
+ * @s: pointer to input string
+ *
+ * Return: returns string length
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - concates two input strings
+ * @s1: string input 1
+ * @s2: string input 2
+ * @n: number of bytes to retrieve from s2
+ * Return: pointer to concatenated string
+ **/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a, *p;
+	unsigned int size, i, j, n1, n2;
+
+	if (s1 == NULL)
+	{
+		n1 = 0;
+	}
+	else
+	{
+		n1 = _strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		n2 = 0;
+	}
+	else
+	{
+		if (n >= _strlen(s2))
+		{
+			n2 = _strlen(s2);
+		}
+		else
+		{
+			n2 = n;
+		}
+	}
+	size = n1 + n2;
+	a = malloc(size * sizeof(char) + 1);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	p = a;
+	for (i = 0; i < n1; i++)
+	{
+		*p++ = *s1++;
+	}
+	for (j = 0; j < n2; j++)
+	{
+		*p++ = *s2++;
+	}
+	*p = '\0';
+	return (a);
+}
