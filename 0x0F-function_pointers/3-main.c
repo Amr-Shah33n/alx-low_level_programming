575bf71655e72488ca167ad708d7a07cbdfc226e
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-19 16:14:40 -0700

    Added check on operator argument size

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 	b = atoi(argv[3]);
 
 	operation = get_op_func(argv[2]);
-	if (operation == NULL)
+	if (operation == NULL || (strlen(argv[2]) != 1))
 	{
 		printf("Error\n");
 		exit(99);
