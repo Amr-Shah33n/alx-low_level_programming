0f43916ab7022643306ed95eb658e1f291d4357e
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-19 15:38:33 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdlib.h>
+
+
+/**
+ * main - check code
+ * @argc: arg count
+ * @argv: array of arguments
+ * Return: exits status 0 on success
+ **/
+int main(int argc, char **argv)
+{
+
+	int a, b, res;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[3] == '0') && (*argv[2] == '%' || *argv[2] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = operation(a, b);
+	printf("%i\n", res);
+	return (0);
+}
