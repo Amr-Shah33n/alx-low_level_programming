404bf8654e3ac83cd530ca9f66afcf87d9fad16e
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-19 16:59:12 -0700

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include<stdlib.h>
 
 /**
  * print_name - print name based on passed function
@@ -9,7 +8,8 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-		return;
-	f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
