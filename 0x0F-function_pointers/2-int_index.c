4c73d325913e0b8a7670cc867c0581a6f3fa8254
amrshaheen <amr.shaheen0000@gmail.com>
2023-04-19 11:10:38 -0700

    Handled null cases for tasks 0, 1 and 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,10 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, val;
 
+	if (size <= 0 || array || cmp)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
 		val = cmp(array[i]);
 		if (val != 0)
 			return (i);
