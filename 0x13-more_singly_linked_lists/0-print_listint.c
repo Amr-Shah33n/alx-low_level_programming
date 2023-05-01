c31ae73d48c655bc1a2de83d03d74cfb45945bba
amrshaheen <amr.shaheen0000@gmail.com>
2023-05-01 06:58:46 -0700

    fixed betty styling

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * size_t_print_listint - prints a list of int values
+ * print_listint - prints a list of int values
  * @h: list input
  * Return: int value number of elements
  **/
@@ -8,7 +8,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
