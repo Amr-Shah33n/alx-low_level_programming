2b4385b50a9d41bbedcb144f691811390ca1f691
amrshaheen <amr.shaheen0000@gmail.com>
2023-05-08 13:22:43 -0700

    Task 1 and 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - reads a text file and prints it to the stdout
+ * append_text_to_file - reads a text file and prints it to the stdout
  * @filename: name of file to be read
  * @text_content: text to write
  * Return: -1 on failure , 1 on success
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	char *tmp;
 	int l = 0, op = 0, wr = 0;
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			tmp++;
 		}
 	}
-	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_WRONLY | O_APPEND);
 	if (op == -1)
 	{
 		return (-1);
